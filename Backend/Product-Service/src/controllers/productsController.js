4e4f80a361cacc92abb783136c978e9771c0258e
EveHackmon <157503186+EveHackmon@users.noreply.github.com>
2025-04-15 20:13:41 +0300

    update6

--- Backend/Product-Service/src/controllers/productsController.js
+++ Backend/Product-Service/src/controllers/productsController.js
@@ -35,15 +35,8 @@ export const readAllProducts = async (req, res) => {
 
 export const readProduct = async (req, res) => {
     try {
-        const { id } = req.params;
-        const product = await Product.findByPk(id);
-
-        if (!product) {
-            return res.status(404).json({ error: `Product with id ${id} not found` });
-        }
-
-        res.status(200).json(product);
+        res.status(200).json('read product controller');
     } catch (error) {
-        res.status(500).json({ error: 'Error fetching product', details: error.message });
+        res.status(500).json({error: 'error from read function', details: error});
     }
 };
