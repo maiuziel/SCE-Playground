075bd38ee4f5da5d05e26f384072e115ceda4c8a
danielbogus99 <danik444@walla.co.il>
2025-04-21 13:49:57 +0300

    Save until now

---
+++ Backend/LeadsService/src/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config(); 
+
+const express = require('express');
+const app = express();
+
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+
+}); 
