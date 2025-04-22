60ec98c7919eeeb361a361e0ea9b34ee3b09d9de
maiuziel <mayuziel3@gmail.com>
2025-04-22 16:57:51 +0300

    add

---
+++ Frontend/src/pages/SupportHistoryPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function SupportHistoryPage() {
+  return (
+    <div className="page-container">
+      <h1 className="page-title">Support Request History</h1>
+      <p className="page-description">
+        Here you can view all the support requests you've submitted.
+      </p>
+      {/* בעתיד: כאן נוסיף טבלה עם הנתונים מהשרת */}
+    </div>
+  );
+}
