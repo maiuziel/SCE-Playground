edde6bb4c6ebde5ba048648c3fda5fbaf68329e9
yeudaba <yeuda4222@gmail.com>
2025-04-23 13:28:43 -0700

    Use environment PORT in service A

--- Backend/ServiceA/src/index.js
+++ Backend/ServiceA/src/index.js
@@ -2,13 +2,13 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
  
 app.post("/ping", async (req, res) => {
-  res.status(500).send("Ping from service A")
+  res.status(200).send("Ping from service A")
 });
 
 app.listen(PORT, () => {
