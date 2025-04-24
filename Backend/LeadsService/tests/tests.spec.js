d19c7e6699ca833a4934f311bb5f8abe0518e8e2
danielbogus99 <danik444@walla.co.il>
2025-04-24 12:52:03 +0300

    TEST

--- Backend/LeadsService/tests/tests.spec.js
+++ Backend/LeadsService/tests/tests.spec.js
@@ -1,24 +1,36 @@
+// leads-service/tests/leads.test.js
+process.env.NODE_ENV = 'test';
+
 import chai from 'chai';
-import chaiHttp from 'chai-http'; // ✅
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const chaiHttp = require('chai-http');
+
 import { app, startTestServer } from './testServer.js';
 
 const { expect } = chai;
-chai.use(chaiHttp); // ⚠️ חובה כדי ש־chai.request יעבוד
+chai.use(chaiHttp);
 
-let server;
+let server = null;
 
-describe('Leads API Tests', () => {
-  before(async () => {
+describe('Leads Service Tests', () => {
+  before(async function() {
+    this.timeout(10000); // הגדרת טיימאאוט למניעת כשל בהתחברות לדאטאבייס
     server = await startTestServer();
+    console.log("Test server is running");
   });
 
   after(async () => {
     server.close();
   });
 
-  it('should return all leads', async () => {
-    const res = await chai.request(app).get('/getall');
-    expect(res.status).to.equal(200);
-    expect(res.body).to.be.an('array');
+  it('should return all leads', (done) => {
+    chai.request(app)
+      .get('/getall')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('array');
+        done();
+      });
   });
 });
