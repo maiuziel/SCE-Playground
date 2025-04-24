da747e38ae095552cf9796868a7cacb236c4abcc
danielbogus99 <danik444@walla.co.il>
2025-04-24 12:54:39 +0300

    one test getall update

--- Backend/LeadsService/tests/tests.spec.js
+++ Backend/LeadsService/tests/tests.spec.js
@@ -1,36 +1,24 @@
-// leads-service/tests/leads.test.js
-process.env.NODE_ENV = 'test';
-
+// tests/leads.test.js
 import chai from 'chai';
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const chaiHttp = require('chai-http');
-
+import request from 'supertest'; // ⬅️ במקום chai-http
 import { app, startTestServer } from './testServer.js';
 
 const { expect } = chai;
-chai.use(chaiHttp);
-
-let server = null;
+let server;
 
 describe('Leads Service Tests', () => {
-  before(async function() {
-    this.timeout(10000); // הגדרת טיימאאוט למניעת כשל בהתחברות לדאטאבייס
+  before(async function () {
+    this.timeout(10000);
     server = await startTestServer();
-    console.log("Test server is running");
   });
 
   after(async () => {
     server.close();
   });
 
-  it('should return all leads', (done) => {
-    chai.request(app)
-      .get('/getall')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.be.an('array');
-        done();
-      });
+  it('should return all leads', async () => {
+    const res = await request(app).get('/leads/getall');
+    expect(res.status).to.equal(200);
+    expect(res.body).to.be.an('array');
   });
 });
