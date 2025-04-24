a4410abe1b6fcb8fef5551b5cbfc57f59c91e098
danielbogus99 <danik444@walla.co.il>
2025-04-24 19:01:40 +0300

    update

--- Backend/GatewayService/src/routes/gatewayRoutes.js
+++ Backend/GatewayService/src/routes/gatewayRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { forwardAuthRequests } from '../controllers/gatewayController.js';
+import { forwardLeadsRequests } from '../controllers/gatewayController.js';
 
 const router = Router();
 
 router.use('/auth', forwardAuthRequests);
-router.get('/ping', ping);
+
 
 router.use('/leads', forwardLeadsRequests);
 export default router;
