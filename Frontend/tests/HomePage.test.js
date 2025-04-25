8cd8c5ae33cc866c79a358afbafded418fb8c02d
David Moalem <davidmoa26@gmail.com>
2025-04-25 17:05:54 +0300

    fix tests

--- Frontend/tests/HomePage.test.js
+++ Frontend/tests/HomePage.test.js
@@ -1,18 +1,19 @@
-// HomePage.test.jsx
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import HomePage from '../src/pages/HomePage.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { StoreProvider } from '../src/store/StoreContext'; 
 
 describe('HomePage', () => {
   test('renders the welcome message', () => {
     render(
-      <BrowserRouter>
-        <HomePage />
-      </BrowserRouter>
+      <StoreProvider>
+        <BrowserRouter>
+          <HomePage />
+        </BrowserRouter>
+      </StoreProvider>
     );
-    // screen is your access point to the rendered DOM
     expect(screen.getByText('Welcome to SCE Software Ltd.')).toBeInTheDocument();
   });
 });
