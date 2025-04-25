c2ad594ab8967d5643fc678002d006b7d805643a
David Moalem <davidmoa26@gmail.com>
2025-04-25 14:36:58 +0300

    fix

--- Backend/GatewayService/src/routes/gatewayRoutes.js
+++ Backend/GatewayService/src/routes/gatewayRoutes.js
@@ -1,10 +1,10 @@
 // gateway-service/src/routes/gatewayRoutes.js
-import { Router } from "express";
-import { forwardAuthRequests } from "../controllers/gatewayController.js";
-import { forwardProductsRequests } from "../controllers/gatewayController.js";
+import { Router } from 'express';
+import { forwardAuthRequests } from '../controllers/gatewayController.js';
+import { forwardProductsRequests } from '../controllers/gatewayController.js';
 const router = Router();
 
 // Forward all /auth/* requests
-router.use("/auth", forwardAuthRequests);
-router.use("/products", forwardProductsRequests);
+router.use('/auth', forwardAuthRequests);
+router.use('/products', forwardProductsRequests);
 export default router;
