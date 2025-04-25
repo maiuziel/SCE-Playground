504bb81dc27d750740bcf2f2477a035dc669afdd
David Moalem <davidmoa26@gmail.com>
2025-04-25 17:02:27 +0300

    finish

--- Frontend/src/pages/ReportsPage.jsx
+++ Frontend/src/pages/ReportsPage.jsx
@@ -1,13 +1,13 @@
-import { useContext } from "react";
-import { StoreContext } from "../store/StoreContext";
-import { Navigate } from "react-router-dom";
+import { useContext } from 'react';
+import { StoreContext } from '../store/StoreContext';
+import { Navigate } from 'react-router-dom';
 
 export default function ReportsPage() {
   const { user, token } = useContext(StoreContext);
-  console.log("User:", JSON.stringify(user), token);
+  console.log('User:', JSON.stringify(user), token);
 
   // If user is not logged in, redirect to /signin
-  if (!user || !token) return <Navigate to="/signin" replace />;
+  if (!user || !token) return <Navigate to='/signin' replace />;
   return (
     <div>
       <h1>Reports Page</h1>
