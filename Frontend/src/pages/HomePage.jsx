d302491b032252549066c8938744a0a006b3a40d
danielbogus99 <danik444@walla.co.il>
2025-05-07 15:53:47 +0300

    fix

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -16,9 +16,25 @@ export default function HomePage() {
       <div className='home-images'>
         <img src='/reports.jpg' alt='Colorful Product 1' onClick={() => navigate('/reports')} />
       </div>
-      <button onClick={() => navigate('/createlead')}  >
-        Go to Leads Module
-      </button>
+      <button
+  onClick={() => navigate('/createlead')}
+  style={{
+    background: 'linear-gradient(to right,rgb(91, 212, 10), #E100FF)',
+    marginTop: '20px',
+    color: 'white',
+    padding: '10px 20px',
+    border: 'none',
+    borderRadius: '25px',
+    fontWeight: 'bold',
+    boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.2)',
+    cursor: 'pointer',
+    transition: 'transform 0.3s ease',
+  }}
+  onMouseEnter={(e) => (e.currentTarget.style.transform = 'scale(1.05)')}
+  onMouseLeave={(e) => (e.currentTarget.style.transform = 'scale(1)')}
+>
+  🚀 Go to Leads Module
+</button>
     </div>
   );
 }
