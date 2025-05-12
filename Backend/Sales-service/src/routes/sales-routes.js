e22ff05f8fda378ff1f950eeeb7c2b2e1f7a0c72
Babilabong <157375371+Babilabong@users.noreply.github.com>
2025-05-12 18:16:43 +0300

    get workers check fix bugs

--- Backend/Sales-service/src/routes/sales-routes.js
+++ Backend/Sales-service/src/routes/sales-routes.js
@@ -3,14 +3,14 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controller/sales-controller');
 
+router.get('/representatives/is-rep', controller.isSalesRep);
 router.post('/', controller.createSale);
 router.get('/', controller.getAllSales);
 router.get('/:id', controller.getSaleById);
 router.put('/:id', controller.updateSale);
 router.delete('/:id', controller.deleteSale);
 
-router.get('/workers', controller.getWorker);
-router.get('/representatives/is-rep', controller.isSalesRep);
+
 
 
 module.exports = router;
