6e533e7ad2bc1b4da6ff4a6e171fc40c8d65edab
Babilabong <157375371+Babilabong@users.noreply.github.com>
2025-05-29 12:20:29 +0300

    fix bugs in leads

--- Backend/GatewayService/src/controllers/gatewayController.js
+++ Backend/GatewayService/src/controllers/gatewayController.js
@@ -82,14 +82,4 @@ const forwardSalesRequests = async (req, res, next) => {
   
 };
 
-/*
-export async function ping(req, res, next) {
-  try {
-    return res.status(200).json({ message: 'pong' });
-  } catch (error) {
-    return next(error);
-  }
-}
-  */
-
 export { forwardAuthRequests,forwardSalesRequests };
