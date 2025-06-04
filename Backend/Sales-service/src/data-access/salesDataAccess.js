856fa2ae3874ebb9cb8caec5597f0a724f973c4b
Babilabong <157375371+Babilabong@users.noreply.github.com>
2025-06-04 21:29:34 +0300

    add leads to our db

--- Backend/Sales-service/src/data-access/salesDataAccess.js
+++ Backend/Sales-service/src/data-access/salesDataAccess.js
@@ -280,16 +280,6 @@ exports.getLeadById = async (phone) => {
   return result.rows[0] || null;
 };
 
-// עדכון ליד קיים (רק סטטוס ותאריך)
-exports.updateLead = async (lead_id, status, application_date) => {
-  await db.query(
-    `UPDATE leads_table 
-     SET status = $1, application_date = $2
-     WHERE lead_id = $3`,
-    [status, application_date, lead_id]
-  );
-};
-
 // הכנסת ליד חדש
 exports.insertLead = async ({ lead_id, status, application_date }) => {
   await db.query(
