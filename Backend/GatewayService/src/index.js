82b3b88b5e07833d2c738bfff0f3fb5db9329231
Dor Maudi <dor.maudi9@gmail.com>
2025-06-11 17:46:08 +0300

    fixed conflicts, and a bug

--- Backend/GatewayService/src/index.js
+++ Backend/GatewayService/src/index.js
@@ -10,11 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 
-app.use(json({ limit: '12mb' }));
-
 app.use(cors());
  
-app.use(express.json()); 
+app.use(express.json({ limit: '12mb' })); 
 
 app.use('/', gatewayRoutes);
 
